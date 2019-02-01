bf067c3e505c6897c34e4fa491622b5edcf7b482
wagner <wagnerricardonet@gmail.com>
2019-01-31 23:12:32 -0200

    aplicando container docker no projeto

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 //Cria a conexão com o Mongo
 mongoose.connect(
-    'mongodb://localhost:27017/auth',
+    'mongodb://127.0.0.1:27017/auth',
     {
         useNewUrlParser: true      
     }
